b7f81fec46ba9dc9d35015171c558e82ed6a988e
shashwat shukla <shuklashashwat009@gmail.com>
2025-08-07 22:55:22 +0530

    cloudinary implementation done

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,8 +1,6 @@
  import mongoose, { Schema } from "mongoose";
  import   jwt from "jsonwebtoken";
  import bcrypt from "bcrypt"
-
-
  const userSchema=new mongoose.Schema({
     username:{
         type:String,
