f0893a1a3e014f89c9a05db9339e256e0f1a259b
shashwat shukla <shuklashashwat009@gmail.com>
2025-08-11 15:08:25 +0530

    router setup

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser';
 
+import userRouter from "./route/user.routes.js"
+
 const app=express();
 app.use(cors({
     origin:process.env.CORS_ORG,
@@ -12,5 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static('public'));
 app.use(cookieParser()); 
+
+
+app.use('/api/v1/user',userRouter);
 
 export {app}
